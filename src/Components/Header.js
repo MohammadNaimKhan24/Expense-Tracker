39a43d3e54f1bbcbb25a8a938e1284d82079dec1
Mohammad Naim Khan <mohammadnaimkhan66@gmail.com>
2021-10-12 00:29:24 +0600

    expense tracker app

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="headerColor">Expense 
+                <span className="trackerColor">
+                Tracker
+                 </span>
+            </h2>
+        </div>
+    );
+};
+
+export default Header;
